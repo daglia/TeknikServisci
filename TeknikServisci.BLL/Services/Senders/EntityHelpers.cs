0096e97d94d4bec232a5c02b744acf7f6b85180f
Emre Çetin <cetinemre17@gmail.com>
2019-02-14 14:05:00 +0300

    Evladiyelik helperlar eklendi.

---
+++ TeknikServisci.BLL/Services/Senders/EntityHelpers.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Validation;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeknikServisci.BLL.Services.Senders
+{
+    public class EntityHelpers
+    {
+        public static string ValidationMessage(DbEntityValidationException ex)
+        {
+            var result = ex.EntityValidationErrors.Aggregate(string.Empty, (accumulator2, validationErrors) => validationErrors.ValidationErrors.Aggregate(accumulator2, (accumulator, validationError) => accumulator += $"Property: {validationError.PropertyName} Error: {validationError.ErrorMessage}\n"));
+            return result + "\n";
+        }
+    }
+}
---
+++ TeknikServisci.BLL/Services/Senders/EnumHelpers.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeknikServisci.BLL.Services.Senders
+{
+    public class EnumHelpers
+    {
+        public static string GetDescription<T>(T item)
+        {
+            var descriptionAttributes = typeof(T)
+                .GetMember(typeof(T)
+                    .GetEnumName(item))[0]
+                .GetCustomAttributes(typeof(DescriptionAttribute), false)[0];
+
+            return ((DescriptionAttribute)descriptionAttributes).Description;
+        }
+    }
+}
