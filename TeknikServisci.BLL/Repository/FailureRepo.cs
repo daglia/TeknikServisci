9940273b369517e343f59d50c4ca3aded5a8b4af
Emre Çetin <cetinemre17@gmail.com>
2019-02-14 14:15:13 +0300

    Filterlar ayarlandı

---
+++ TeknikServisci.BLL/Repository/FailureRepo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TeknikServisci.Models.Entities;
+
+namespace TeknikServisci.BLL.Repository
+{
+    public class FailureRepo : RepositoryBase<Failure, int>
+    {
+    }
+}
---
+++ TeknikServisci/App_Start/FilterConfig.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TeknikServisci.App_Code;
+
+namespace TeknikServisci.App_Start
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+            filters.Add(new ExceptionHandlerFilterAttribute());
+        }
+    }
+}
