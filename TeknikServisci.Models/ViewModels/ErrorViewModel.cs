0fb74e7579813aedde29fda8a581eb2d94eb1dc6
Alper Dağlı <alper379@gmail.com>
2019-02-14 14:42:42 +0300

    Hata sayfası eklendi.

---
+++ TeknikServisci.Models/ViewModels/ErrorViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeknikServisci.Models.ViewModels
+{
+    public class ErrorViewModel
+    {
+        public string Text { get; set; }
+        public string ControllerName { get; set; }
+        public string ActionName { get; set; }
+        public int ErrorCode { get; set; }
+    }
+}
--- TeknikServisci/Controllers/ErrorController.cs
+++ TeknikServisci/Controllers/ErrorController.cs
@@ -11,14 +11,6 @@ namespace TeknikServisci.Controllers
         // GET: Error
         public ActionResult Index()
         {
-            Response.Status = "Aradığınız sayfa bulunamadı";
-            Response.StatusCode = 404;
-            return View();
-        }
-
-        public ActionResult H500()
-        {
-            Response.StatusCode = 500;
             return View();
         }
     }
