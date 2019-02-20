063ac993950146a3a4be38df0b45f14866ba3faf
Alper Dağlı <alper379@gmail.com>
2019-02-20 11:40:10 +0300

    Kullanıcı listesi güncellemesi. #2

--- TeknikServisci.Models/Enums/IdentityRoles.cs
+++ TeknikServisci.Models/Enums/IdentityRoles.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,9 +9,13 @@ namespace TeknikServisci.Models.Enums
 {
     public enum IdentityRoles
     {
+        [Description("Yönetici")]
         Admin,
+        [Description("Müşteri")]
         User,
+        [Description("Teknisyen")]
         Technician,
+        [Description("Operatör")]
         Operator
     }
 }
